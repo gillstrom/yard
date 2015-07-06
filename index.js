8b44959c00df04d82fca2491f6efc4ce04f7a309
Andreas Gillström <andreasgillstrom@gmail.com>
2015-07-06 07:18:34 +0200

    Add check for type

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 module.exports = function (yard) {
+	if (typeof yard !== 'number') {
+		throw new TypeError('Expected a number');
+	}
+
 	return {
 		cm: yard * 91.44,
 		in: yard * 36,
